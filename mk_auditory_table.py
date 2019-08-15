c9ba08bc8bc2516375b7e5363f13f88a3e99b429
Carrielin <30627002+carrielin32@users.noreply.github.com>
2019-08-15 15:00:57 +0200

    Update mk_auditory_table.py

--- mk_auditory_table.py
+++ mk_auditory_table.py
@@ -10,18 +10,3 @@ df=df.fillna('0')
 
 df=df.to_csv('/Users/carrielin/Downloads/session1_transform.csv')
 
-
-# convert to array first
-
-#df=np.array(df)
-
-#df=np.savetxt('/Users/carrielin/Downloads/session1_final.txt')
-
-
-#import csv
-csv_file = input('/Users/carrielin/Downloads/session1_final.csv')
-txt_file = input('/Users/carrielin/Downloads/session1_final.txt')
-with open(txt_file, "w") as my_output_file:
-    with open(csv_file, "r") as my_input_file:
-        [ my_output_file.write(" ".join(row)+'\n') for row in csv.reader(my_input_file)]
-    my_output_file.close()
