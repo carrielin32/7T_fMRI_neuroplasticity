f5bd4659c6252f60274655cbf1c7315367e7a9c4
Carrielin <30627002+carrielin32@users.noreply.github.com>
2019-08-15 14:32:56 +0200

    Add files via upload

---
+++ replace_con_new.py
@@ -0,0 +1,58 @@
+import sys
+from pandas import Series, DataFrame
+import pandas as pd
+import numpy as np 
+
+#parsed=pd.read_csv('/Users/carrielin/Desktop/exp_Visual_Audi/log_file_auditory/session1.csv')
+
+parsed=pd.read_csv('/Users/carrielin/Desktop/V8678_log/log1.csv')
+
+/Users/carrielin/Downloads/v3211_log1.csv 
+
+#indexNames = parsed[(parsed['con'] == 1) | (parsed['con']== 11) | (parsed['con'] == 49) | (parsed['con'] == 66)].index
+#parsed=parsed.drop(indexNames, inplace=True)
+
+#use this method 
+replace_values = {'1':0, '11':0, '49':0,'66':0}
+parsed = parsed.replace({"con":replace_values})
+parsed = parsed[parsed['con']!=0]
+
+#or this section
+replace_values = {'2':0, '11':0, '49':0,'67':0}
+parsed = parsed.replace({"con":replace_values})
+parsed = parsed[parsed['con']!=0]
+
+#or this section 
+replace_values = {'1':0, '2':0, '11':0, '49':0,'66':0, '67':0}
+parsed = parsed.replace({"con":replace_values})
+parsed = parsed[parsed['con']!=0]
+
+
+# 3k left
+parsed['con_new']=parsed['con'].replace('long, 3k, L','3kL')
+parsed['con_new']=parsed['con_new'].replace('short, 3k, L','3kL')
+
+# 3k right
+parsed['con_new']=parsed['con_new'].replace('long, 3k, R','3kR')
+parsed['con_new']=parsed['con_new'].replace('short, 3k, R','3kR')
+
+# 1k left
+parsed['con_new']=parsed['con_new'].replace('long, 1k, L','1kL')
+parsed['con_new']=parsed['con_new'].replace('short, 1k, L','1kL')
+
+# 1k right
+parsed['con_new']=parsed['con_new'].replace('long, 1k, R','1kR')
+parsed['con_new']=parsed['con_new'].replace('short, 1k, R','1kR')
+
+# 8k left
+parsed['con_new']=parsed['con_new'].replace('long, 8k, L','8kL')
+parsed['con_new']=parsed['con_new'].replace('short, 8k, L','8kL')
+
+# 8k right
+parsed['con_new']=parsed['con_new'].replace('long, 8k, R','8kR')
+parsed['con_new']=parsed['con_new'].replace('short, 8k, R','8kR')
+
+parsed.to_csv('/Users/carrielin/Desktop/exp_Visual_Audi/log_file_auditory/session1_new_0308.csv')
+
+
+# similar as session2
